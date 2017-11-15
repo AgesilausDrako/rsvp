e25f0dce1d7254c104ccb75c25d4bbc80408b225
AgesilausDrako <agesilaus@yahoo.com>
2017-11-15 16:38:38 -0500

    added toggleConfirmation function

--- rsvpapp/src/App.js
+++ rsvpapp/src/App.js
@@ -20,6 +20,19 @@ class App extends Component {
     ]
   }
 
+  toggleConfirmationAt = indexToChange => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+       if (index === indexToChange) {
+        return {
+          ...guest,
+          isConfirmed: !guest.isConfirmed
+        };
+       }
+       return guest;
+      })
+    });
+
   getTotalInvited = () => this.state.guests.length;
   // getAttendingGuests = () =>
   // getUnconfirmedGuests = () =>
@@ -58,7 +71,9 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-        <GuestList guests={this.state.guests}/>
+        <GuestList 
+          guests={this.state.guests}
+          toggleConfirmationAt={this.toggleConfirmationAt}/>
       </div>
     </div>
     );
